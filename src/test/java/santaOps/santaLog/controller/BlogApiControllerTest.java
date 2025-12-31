8e5779be05a7f2875115ec3e81fad8994cbd8aa9
chaeyeonKong <dnjftjd53@naver.com>
2025-12-31 14:54:12 +0900

    test: 게시글 수정 기능 테스트 #1

--- src/test/java/santaOps/santaLog/controller/BlogApiControllerTest.java
+++ src/test/java/santaOps/santaLog/controller/BlogApiControllerTest.java
@@ -136,4 +136,36 @@ class BlogApiControllerTest {
         assertThat(articles).isEmpty();
     }
 
+    @DisplayName("updateArticle: 게시글 수정 성공")
+    @Test
+    public void updateArticle() throws Exception{
+        //given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content).build());
+
+        final String newTitle = "new title";
+        final String newContent = "new content";
+
+        Article request = new Article(newTitle, newContent);
+
+        //when
+        final ResultActions result = mockMvc.perform(put(url, savedArticle.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)));
+
+        //then
+        result.andExpect(status().isOk());
+
+        Article article = blogRepository.findById(savedArticle.getId()).get();
+
+        assertThat(article.getTitle()).isEqualTo(newTitle);
+        assertThat(article.getContent()).isEqualTo(newContent);
+
+    }
+    
+
 }
