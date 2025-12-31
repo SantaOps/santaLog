759b52b45471bb7b682c19f5578f3215ef22a30a
chaeyeonKong <dnjftjd53@naver.com>
2025-12-31 15:28:19 +0900

    feat: 전체 게시글 리스트 화면 구현 #7

---
+++ src/main/java/santaOps/santaLog/dto/ArticleListViewResponse.java
@@ -0,0 +1,21 @@
+package santaOps.santaLog.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import santaOps.santaLog.domain.Article;
+
+@Getter
+@RequiredArgsConstructor
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article){
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+
+}
