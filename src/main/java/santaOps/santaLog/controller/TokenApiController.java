873b6df7caa44abdf93bf1954d8d835c705739e4
chaeyeonKong <dnjftjd53@naver.com>
2026-01-07 23:45:09 +0900

    feat: JWT 구현 완료 #12

---
+++ src/main/java/santaOps/santaLog/controller/TokenApiController.java
@@ -0,0 +1,26 @@
+package santaOps.santaLog.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import santaOps.santaLog.dto.CreateAccessTokenRequest;
+import santaOps.santaLog.dto.CreateAccessTokenResponse;
+import santaOps.santaLog.service.TokenService;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request){
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+
+}
