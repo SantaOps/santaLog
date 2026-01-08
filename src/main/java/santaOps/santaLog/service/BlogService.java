e35fa75e5cd3e0730ac2ff91a6a86645334bd5d9
chaeyeonKong <dnjftjd53@naver.com>
2026-01-08 23:14:01 +0900

    feat: 구글 소셜 로그인 구현 완료 #16

--- src/main/java/santaOps/santaLog/service/BlogService.java
+++ src/main/java/santaOps/santaLog/service/BlogService.java
@@ -1,13 +1,13 @@
 package santaOps.santaLog.service;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import santaOps.santaLog.domain.Article;
 import santaOps.santaLog.dto.AddArticleRequest;
+import santaOps.santaLog.dto.UpdateArticleRequest;
 import santaOps.santaLog.repository.BlogRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -16,8 +16,9 @@ public class BlogService {
 
     private final BlogRepository blogRepository;
 
-    public Article save(Article request){
-        return blogRepository.save(request);
+
+    public Article save(AddArticleRequest request, String userName) {
+        return blogRepository.save(request.toEntity(userName));
     }
 
     public List<Article> findAll() {
@@ -29,16 +30,28 @@ public class BlogService {
     }
 
     public void delete(long id){
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found: " + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
-    @Transactional
-    public Article update(long id, Article request){
-        Article article = blogRepository.findById(id).orElseThrow(()->new IllegalArgumentException("not fond: "+ id));
-        article.setTitle(request.getTitle());
-        article.setContent(request.getContent());
-        article.setUpdateAt(LocalDateTime.now());
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+
+        authorizeArticleAuthor(article);
+        article.update(request.getTitle(), request.getContent());
+
         return article;
+    }
+
+    private static void authorizeArticleAuthor(Article article){
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!article.getAuthor().equals(userName)){
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 
 
