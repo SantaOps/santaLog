67e2cb8dbc7cff8b484c8f0cc032dda4cbeb467a
chaeyeonKong <dnjftjd53@naver.com>
2026-01-22 20:15:04 +0900

    feat: image path를 aws s3로 변경 #54

--- santaLog-article/src/main/java/santaOps/santaLog/config/WebMvcConfig.java
+++ santaLog-article/src/main/java/santaOps/santaLog/config/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package santaOps.santaLog.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,13 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Value("${upload.path}")
-    private String uploadPath;
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**")
-                .addResourceLocations(uploadPath)
                 .addResourceLocations("classpath:/static/img/");
     }
 }
